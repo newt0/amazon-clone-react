8fa985bd72bf7ebafcf28369e2768f89292774f0
newt0 <py_lambda@yahoo.co.jp>
2020-09-12 15:23:50 +0900

    add: react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <Router>
+      {" "}
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Router path="/login"><h1>Login page</h1></Router>
+          <Route path="/"><h1>Top Page</h1></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
